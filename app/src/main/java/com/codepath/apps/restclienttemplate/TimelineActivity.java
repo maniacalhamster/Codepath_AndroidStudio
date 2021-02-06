03002b19704b8f49e50f7aed10452ae9ad52a2bd
Akar Singh <aks002@ucsd.edu>
2021-02-05 18:28:38 -0800

    Button to compose a tweet created and responds to tap with toast!

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -7,6 +7,9 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -96,6 +99,25 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.i(TAG, "onFailure for loadMoreData!", throwable);
             }
         }, tweets.get(tweets.size()-1).id);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose){
+            // Compose icon has been tapped
+            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
+
+            // Navigate to the compose activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeLine() {
