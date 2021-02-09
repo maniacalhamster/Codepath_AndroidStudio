1e320887e66abdc18ec34cf489b339a2306b15c2
Akar Singh <aks002@ucsd.edu>
2021-02-08 19:12:39 -0800

    On main activity, queries posts using the parse model defined for posts in the background. Only logs description and author's username for now

--- app/src/main/java/com/example/finstagram/MainActivity.java
+++ app/src/main/java/com/example/finstagram/MainActivity.java
@@ -8,6 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
@@ -27,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
         etCaption = findViewById(R.id.etCaption);
         btnPost = findViewById(R.id.btnPost);
         
+        queryPosts();
+        
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts: ", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: "+post.getDescription() + " - by: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
