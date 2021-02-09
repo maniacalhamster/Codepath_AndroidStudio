020efd79dedafa0e75cb27ec5e1230b0a1b3dc49
Akar Singh <aks002@ucsd.edu>
2021-02-08 17:29:04 -0800

    User can login and head to main activity (connection with Parse server succesful)  - going back does not return to login  - if user already logged in, no need to login again

--- app/src/main/java/com/example/finstagram/LoginActivity.java
+++ app/src/main/java/com/example/finstagram/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.example.finstagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +27,10 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if(ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -38,6 +48,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "Attempting to login user: "+username);
-        // TODO: navigate to main activity if user signs in correctly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    // TODO: better error handling
+                    Log.e(TAG, "Issue with login: ", e);
+                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/finstagram/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.finstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("hSbbJh9JDV8ZwCBIvzzTuU0RNmTvT1CtX2Au92lq")
+                .clientKey("CdsgyjripCas60DbQi9qPPbatmvdc6kwLN0PPmUh")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
