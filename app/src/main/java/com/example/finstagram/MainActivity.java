a5fb059e734cdfd56d3d540041100c489984da1e
Akar Singh <aks002@ucsd.edu>
2021-02-08 18:38:21 -0800

    UI for main activity setup - scaffolding for taking a picture, writing a caption, and submitting a post setup

--- app/src/main/java/com/example/finstagram/MainActivity.java
+++ app/src/main/java/com/example/finstagram/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.finstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    private ImageView ivPicture;
+    private Button btnCamera;
+    private EditText etCaption;
+    private Button btnPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivPicture = findViewById(R.id.ivPicture);
+        btnCamera = findViewById(R.id.btnCamera);
+        etCaption = findViewById(R.id.etCaption);
+        btnPost = findViewById(R.id.btnPost);
+        
     }
 }
