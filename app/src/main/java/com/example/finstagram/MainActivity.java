7988c0402332110462e07cae823f1b9bd03f9d08
Akar Singh <aks002@ucsd.edu>
2021-02-12 09:39:13 -0800

    Description can be written to backend.

--- app/src/main/java/com/example/finstagram/MainActivity.java
+++ app/src/main/java/com/example/finstagram/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.util.Currency;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,8 +38,38 @@ public class MainActivity extends AppCompatActivity {
         etCaption = findViewById(R.id.etCaption);
         btnPost = findViewById(R.id.btnPost);
         
-        queryPosts();
+//      queryPosts();
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String caption = etCaption.getText().toString();
+                if(caption.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Caption cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(caption, currentUser);
+            }
+        });
         
+    }
+
+    private void savePost(String caption, ParseUser currentUser) {
+        Post post = new Post();
+        post.setDescription(caption);
+//        post.setImage();
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error while saving: ", e);
+                    Toast.makeText(MainActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+                }
+                Log.i(TAG, "Post save was successful!!");
+                etCaption.setText("");
+            }
+        });
     }
 
     private void queryPosts() {
